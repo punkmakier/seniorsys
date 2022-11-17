7e50d04e6bf0a24458c2b735de180f968580bbf0
punkmakier <69425047+punkmakier@users.noreply.github.com>
2022-11-17 19:40:55 +0800

    partial system

--- Android/AndroidFunction.php
+++ Android/AndroidFunction.php
@@ -1,24 +1,38 @@
 <?php
     require_once 'AndroidRequest.php';
     require_once '../Model/SeniorRegistration.php';
+    require_once '../Model/UpdateBasicInfo.php';
+    require_once '../Model/PartnerInformation.php';
+   
 
-
+    $androidRequest = new AndroidRequest;
     
     if(isset($_POST['loginUsername']) && isset($_POST['loginPassword'])){
         $uname = $_POST['loginUsername'];
         $pass = $_POST['loginPassword'];
-        $androidRequest = new AndroidRequest;
+        
         $androidRequest->androidLogin($uname,md5($pass));
+    }
+
+    else if(isset($_GET['userIDAccountMessage'])){
+        $androidRequest->showCompleteName($_GET['userIDAccountMessage']);
     }
 
     else if(isset($_GET['userIDAccount'])){
         $uniqueID = $_GET['userIDAccount'];
-        $androidRequest = new AndroidRequest;
         $androidRequest->HomeDisplayUserInfo($uniqueID);
     }
 
+    else if(isset($_GET['checkAllStatus'])){
+        $uniqueID = $_GET['checkAllStatus'];
+        $androidRequest->viewAllStatusData($uniqueID);
+    }
+
+
+
+
     else if(isset($_POST['reg_fname']) && isset($_POST['reg_mname']) && isset($_POST['reg_lname']) && isset($_POST['reg_bday']) && isset($_POST['reg_cp']) && isset($_POST['reg_email']) && isset($_POST['reg_addr']) && 
-    isset($_POST['reg_birthCert']) && isset($_POST['reg_brgy']) && isset($_POST['reg_usern']) && isset($_POST['reg_pass'])){
+        isset($_POST['reg_birthCert']) && isset($_POST['reg_brgy']) && isset($_POST['reg_usern']) && isset($_POST['reg_pass'])){
         $lname = $_POST['reg_lname'];
         $fname = $_POST['reg_fname'];
         $mname = $_POST['reg_mname'];
@@ -51,9 +65,127 @@
             
         }
 
-    }else{
-        echo "Ops! There's something wrong.";
     }
+
+  
+    else if(isset($_POST['messageTitle']) && isset($_POST['messageDescription']) && isset($_POST['messageFullname'])){
+        $title = $_POST['messageTitle'];
+        $desc = $_POST['messageDescription'];
+        $fname = $_POST['messageFullname'];
+        $androidRequest->androidSendMssg($fname,$title,$desc);
+    }
+
+    else if(isset($_GET['accountDatatoUpdate'])){
+        $androidRequest->androidUserInfoShowToUpdate($_GET['accountDatatoUpdate']);
+    }
+
+
+    else if(isset($_GET['checkOldPassUID']) && isset($_GET['oldpass'])  && isset($_GET['newpass']) && isset($_GET['username']) && isset($_GET['email'])){
+        $androidRequest->androidCheckOldPass($_GET['oldpass'],md5($_GET['newpass']),$_GET['username'],$_GET['email'],$_GET['checkOldPassUID']);
+    }
+
+    
+    else if(isset($_GET['basicInfoUpdateID'])){
+        $uid = $_GET['basicInfoUpdateID'];
+        $androidRequest->viewBasicAllInfoByUser($uid);
+    }
+
+    else if(isset($_POST['update_basic_uid'])){
+        $fname = $_POST['update_basic_fname'];
+        $lname = $_POST['update_basic_lname'];
+        $mname = $_POST['update_basic_mname'];
+        $address = $_POST['update_basic_addr'];
+        $birhtday = $_POST['update_basic_bday'];
+        $placeofbirth = $_POST['update_basic_pob'];
+        $age = $_POST['update_basic_age'];
+        $gender = $_POST['update_basic_gender'];
+        $educattain = $_POST['update_basic_educat'];
+        $voters = $_POST['update_basic_voter'];
+        $civilstat = $_POST['update_basic_civil'];
+        $citizenship = $_POST['update_basic_citi'];
+        $religion = $_POST['update_basic_rel'];
+        $cpno = $_POST['update_basic_cp'];
+        $telno = $_POST['update_basic_tpno'];
+        $occu = $_POST['update_basic_occu'];
+        $monthly = $_POST['update_basic_month'];
+        $uniqueID = $_POST['update_basic_uid'];
+        $basicInfoStatus = "Yes";
+        $update = new UpdateBasicInfo(trim($fname),trim($lname),trim($mname),$address,$birhtday,$placeofbirth,$age,$gender,$educattain,$voters,$civilstat,$citizenship,$religion,$cpno,$telno,$occu,$monthly,$uniqueID,$basicInfoStatus);
+        if($update->updateBasicInfo()){
+            echo "Success";
+        }
+        else{
+            echo "Failed";
+        }
+    }
+
+    else if(isset($_GET['partnerInfoUpdateID'])){
+        $uid = $_GET['partnerInfoUpdateID'];
+        $androidRequest->viewPartnerAllInfoByUser($uid);
+    }
+
+    else if(isset($_POST['update_partner_uid'])){
+        $lname = $_POST['update_partner_fname'];
+        $fname = $_POST['update_partner_lname'];
+        $mname = $_POST['update_partner_mname'];
+        $birthDate = $_POST['update_partnerbday'];
+        $age = $_POST['update_partner_age'];
+        $edAttain = $_POST['update_partner_educat'];
+        $occu = $_POST['update_partner_occu'];
+        $monthly = $_POST['update_partner_month'];
+        $uniqueID = $_POST['update_partner_uid'];
+        $partnerInfo = "Yes";
+
+        $partner = new PartnerInformation($lname,$fname,$mname,$birthDate,$age,$edAttain,$occu,$monthly,$uniqueID,$partnerInfo);
+        if($partner->updatePartnerInfo()){
+            echo "Success";
+        }else{
+            echo "Failed";
+        }
+    }
+
+    else if(isset($_GET['healthIssueUpdateID'])){
+        $uid = $_GET['healthIssueUpdateID'];
+        $androidRequest->SnrHSCMIC($uid);
+    }
+
+    else if(isset($_POST['update_healthiss_fname'])){
+
+        $uid = $_POST['update_healthiss_uid'];
+        $cmic = $_POST['update_healthiss_fname'];
+        if($androidRequest->updateHealthIssue($cmic,$uid)){
+            echo "Success";
+        }else {echo "Failed";}
+    }
+
+
+
+
+    else if(isset($_GET['emergencyInfoUpdateID'])){
+        $uid = $_GET['emergencyInfoUpdateID'];
+        $androidRequest->viewEmergencyAllInfoByUser($uid);
+        
+    }
+
+    else if(isset($_POST['update_emergency_uid'])){
+        $uid = $_POST['update_emergency_uid'];
+        $fname = $_POST['update_emergency_fname'];
+        $lname = $_POST['update_emergency_lname'];
+        $mname = $_POST['update_emergency_mname'];
+        $addr = $_POST['update_emergency_addr'];
+        $age = $_POST['update_emergency_age'];
+
+        if($androidRequest->updateEmergencyContact($lname,$fname,$mname,$addr,$age,$uid)){
+            echo "Success";
+        }else {echo "Failed";}
+
+    }
+
+    
+
+
+    
+
 
 
 ?>
--- Android/AndroidRequest.php
+++ Android/AndroidRequest.php
@@ -11,7 +11,21 @@
             $stmt->execute();
             if($stmt->rowCount() > 0){
                 while($res = $stmt->fetch()){
-                    echo $res["UserUniqueID"];
+                    echo  $res['UserUniqueID'];
+                }
+                return true;
+            }
+            else{
+                echo "Failed";
+            }
+        }
+        public function showCompleteName($id){
+            $con = $this->openConnection();
+            $stmt = $con->prepare("SELECT `FirstName`,`MiddleName`,`LastName` FROM `srpersonalinfo` WHERE `UserUniqueID`='$id'");
+            $stmt->execute();
+            if($stmt->rowCount() > 0){
+                while($res = $stmt->fetch()){
+                    echo  $res['FirstName']." ".substr($res['MiddleName'], 0,1).". ".$res['LastName'];
                 }
                 return true;
             }
@@ -46,6 +60,236 @@
                 echo "Failed";
             }
         }
+
+        public function androidSendMssg($fname,$title,$desc){
+            $con = $this->openConnection();
+            $stmt = $con->prepare("INSERT INTO `message` (`Subject`,`Description`,`MessageBy`) VALUES('$title','$desc','$fname')");
+            if($stmt->execute()){
+                echo "Message Sent";
+            }else{
+                echo "Failed";
+            }
+    
+        }
+
+
+        public function androidUserInfoShowToUpdate($uniqueID){
+            $con = $this->openConnection();
+            $stmt = $con->prepare("SELECT `Username`, `Email` FROM srpersonalinfo WHERE `UserUniqueID`='$uniqueID'");
+            $stmt->execute();
+            $accountInfoObj = array();
+            if($stmt->rowCount() > 0){
+                while($res = $stmt->fetch()){
+                    header("Content-Type: JSON");
+                    $accountInfoObj[] = array(
+                        "Username" => $res['Username'],
+                        "Email" => $res['Email']
+                    );
+
+                    echo json_encode($accountInfoObj, JSON_PRETTY_PRINT);
+                   
+                }
+                return true;
+            }
+            else{
+                echo "Failed";
+            }
+        }
+
+
+
+
+
+
+        public function androidCheckOldPass($oldpass,$newPass,$uname,$email,$userid){
+            $con = $this->openConnection();
+            $stmt = $con->prepare("SELECT `Password` FROM `srpersonalinfo` WHERE `UserUniqueID`='$userid'");
+            $stmt->execute();
+            if($stmt->rowCount() > 0){
+                while($res = $stmt->fetch()){
+                    if($res['Password'] == md5($oldpass)){
+                       $stmt2 = $con->prepare("UPDATE `srpersonalinfo` SET `Password`='$newPass', `Username`='$uname',`Email`='$email' WHERE `UserUniqueID`='$userid'");
+                        if($stmt2->execute()){
+                            echo "Success";
+                        }
+                    }else{
+                        echo "Incorrect";
+                    }
+                }
+            }
+            
+    
+        }
+
+        public function androidUpdateAccount($uname,$email,$password,$userid){
+            $con = $this->openConnection();
+            $stmt = $con->prepare("UPDATE `srpersonalinfo` SET `Username`='$uname', `Email`='$email',`Password`='$password' WHERE `UserUniqueID`='$userid'");
+            if($stmt->execute()){
+                echo "Success";
+            }else{
+                echo "Failed";
+            }
+    
+        }
+
+        public function viewBasicAllInfoByUser($uid){
+            $con = $this->openConnection();
+            $stmt = $con->prepare("SELECT * FROM `srpersonalinfo` WHERE `UserUniqueID`='$uid'");
+            $stmt->execute();
+            $showData = array();
+            if($stmt->rowCount() > 0){
+                while($res = $stmt->fetch()){
+                    header("Content-Type: JSON");
+                    $showData[] = array(
+                        "FirstName" => $res['FirstName'],
+                        "MiddleName" => $res['MiddleName'],
+                        "LastName" => $res['LastName'],
+                        "Address" => $res['Address'],
+                        "Birthdate" => $res['Birthdate'],
+                        "PlaceOfBirth" => $res['PlaceOfBirth'],
+                        "Age" => $res['Age'],
+                        "Gender" => $res['Gender'],
+                        "EducAttainment" => $res['EducAttainment'],
+                        "VoterStatus" => $res['VoterStatus'],
+                        "CivilStatus" => $res['CivilStatus'],
+                        "Citizenship" => $res['Citizenship'],
+                        "Religion" => $res['Religion'],
+                        "CellphoneNo" => $res['CellphoneNo'],
+                        "TeleNumber" => $res['TeleNumber'],
+                        "Occupation" => $res['Occupation'],
+                        "MonthlyIncome" => $res['MonthlyIncome']
+                    );
+                    echo json_encode($showData, JSON_PRETTY_PRINT);
+                }
+            }else{
+                echo "Failed";
+            }
+        }
+
+
+
+
+        public function viewPartnerAllInfoByUser($uid){
+            $con = $this->openConnection();
+            $stmt = $con->prepare("SELECT * FROM `srparterinfo` WHERE `UserUniqueID`='$uid'");
+            $stmt->execute();
+            $showData = array();
+            if($stmt->rowCount() > 0){
+                while($res = $stmt->fetch()){
+                    header("Content-Type: JSON");
+                    $showData[] = array(
+                        "FirstName" => $res['FirstName'],
+                        "MiddleName" => $res['MiddleName'],
+                        "LastName" => $res['LastName'],
+                        "Birthdate" => $res['Birthdate'],
+                        "Age" => $res['Age'],
+                        "EducAttainment" => $res['EducAttainment'],
+                        "Occupation" => $res['Occupation'],
+                        "MonthlyIncome" => $res['MonthlyIncome']
+                    );
+                    echo json_encode($showData, JSON_PRETTY_PRINT);
+                }
+            }else{
+                echo "Failed";
+            }
+        }
+
+        public function viewAllStatusData($uid){
+            $con = $this->openConnection();
+            $stmt = $con->prepare("SELECT p.BasicInfoStatus, pr.SrPartnerInfoStatus, h.SrHealthIssueStatus, e.SrEmerContactStatus FROM `srpersonalinfo` as p INNER JOIN `srparterinfo` as pr on p.`UserUniqueID` = pr.UserUniqueID INNER JOIN `srhealthissue` as h ON p.`UserUniqueID` = h.UserUniqueID INNER JOIN `sremercontact` as e ON p.UserUniqueID = e.UserUniqueID WHERE p.UserUniqueID = '$uid';");
+            $stmt->execute();
+            $showData = array();
+            if($stmt->rowCount() > 0){
+                while($res = $stmt->fetch()){
+                    header("Content-Type: JSON");
+                    $showData[] = array(
+                        "BasicInfoStatus" => $res['BasicInfoStatus'],
+                        "SrPartnerInfoStatus" => $res['SrPartnerInfoStatus'],
+                        "SrHealthIssueStatus" => $res['SrHealthIssueStatus'],
+                        "SrEmerContactStatus" => $res['SrEmerContactStatus'],
+                    );
+                    echo json_encode($showData, JSON_PRETTY_PRINT);
+                }
+            }else{
+                echo "Failed";
+            }
+        }
+
+        
+
+
+
+        public function SnrHSCMIC($uniqueID){
+            $con = $this->openConnection();
+            $sqlQuery = "SELECT `CMIC` FROM `srhealthissue` WHERE `UserUniqueID`='$uniqueID'";
+            $stmt = $con->prepare($sqlQuery);
+            $stmt->execute();
+                if($stmt->rowCount() > 0){
+                    while($res = $stmt->fetch()){
+                        echo $res['CMIC'];
+                    }
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+
+            public function updateHealthIssue($CMIC,$uniqueID){
+                $con = $this->openConnection();
+                $sqlQuery = "UPDATE `srhealthissue` SET `CMIC`='$CMIC', `DateUpdated`= now(),`SrHealthIssueStatus`='Yes' WHERE `UserUniqueID` = '$uniqueID'";
+                $stmt = $con->prepare($sqlQuery);
+                
+                if($stmt->execute()){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+
+
+        
+
+        public function viewEmergencyAllInfoByUser($uid){
+            $con = $this->openConnection();
+            $stmt = $con->prepare("SELECT * FROM `sremercontact` WHERE `UserUniqueID`='$uid'");
+            $stmt->execute();
+            $showData = array();
+            if($stmt->rowCount() > 0){
+                while($res = $stmt->fetch()){
+                    header("Content-Type: JSON");
+                    $showData[] = array(
+                        "FirstName" => $res['FirstName'],
+                        "MiddleName" => $res['MiddleName'],
+                        "Lastname" => $res['Lastname'],
+                        "Address" => $res['Address'],
+                        "CellphoneNumber" => $res['CellphoneNumber'],
+ 
+                    );
+                    echo json_encode($showData, JSON_PRETTY_PRINT);
+                }
+            }else{
+                echo "Failed";
+            }
+        }
+
+        public function updateEmergencyContact($lname,$fname,$mname,$addr,$cp,$uid){
+            $con = $this->openConnection();
+            $sqlQuery = "UPDATE `sremercontact` SET `Lastname`='$lname',`FirstName`='$fname',`MiddleName`='$mname',`Address`='$addr',`CellphoneNumber`='$cp',
+            `DateUpdated`= now(),`SrEmerContactStatus`='Yes' WHERE `UserUniqueID`='$uid'";
+            $stmt = $con->prepare($sqlQuery);
+            if($stmt->execute()){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+
+        
+
+
+
 
     }
 
